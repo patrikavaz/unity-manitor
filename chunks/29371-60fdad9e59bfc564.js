4fca33601141b8a65cc7d0e2dba7b9583a47f7e7
patrikavaz <125215550+patrikavaz@users.noreply.github.com>
2026-01-29 02:27:12 +0000

    unity.com JS updated

---
+++ chunks/29371-60fdad9e59bfc564.js
@@ -0,0 +1,433 @@
+try {
+    ! function() {
+        var e = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof globalThis ? globalThis : "undefined" != typeof self ? self : {},
+            t = (new e.Error).stack;
+        t && (e._sentryDebugIds = e._sentryDebugIds || {}, e._sentryDebugIds[t] = "8be471d9-2e6b-48d0-8a1e-d43f8eec095b", e._sentryDebugIdIdentifier = "sentry-dbid-8be471d9-2e6b-48d0-8a1e-d43f8eec095b")
+    }()
+} catch (e) {}! function() {
+    try {
+        var e = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof globalThis ? globalThis : "undefined" != typeof self ? self : {};
+        e._sentryModuleMetadata = e._sentryModuleMetadata || {}, e._sentryModuleMetadata[(new e.Error).stack] = function(e) {
+            for (var t = 1; t < arguments.length; t++) {
+                var r = arguments[t];
+                if (null != r)
+                    for (var n in r) r.hasOwnProperty(n) && (e[n] = r[n])
+            }
+            return e
+        }({}, e._sentryModuleMetadata[(new e.Error).stack], {
+            "_sentryBundlerPluginAppKey:hexagon-web": !0
+        })
+    } catch (e) {}
+}(), (self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+    [29371], {
+        85: (e, t, r) => {
+            e.exports = r(9593).Symbol
+        },
+        4137: (e, t, r) => {
+            var n = r(85),
+                o = Object.prototype,
+                u = o.hasOwnProperty,
+                l = o.toString,
+                a = n ? n.toStringTag : void 0;
+            e.exports = function(e) {
+                var t = u.call(e, a),
+                    r = e[a];
+                try {
+                    e[a] = void 0;
+                    var n = !0
+                } catch (e) {}
+                var o = l.call(e);
+                return n && (t ? e[a] = r : delete e[a]), o
+            }
+        },
+        8472: e => {
+            e.exports = function(e) {
+                return null != e && "object" == typeof e
+            }
+        },
+        9593: (e, t, r) => {
+            var n = r(39360),
+                o = "object" == typeof self && self && self.Object === Object && self;
+            e.exports = n || o || Function("return this")()
+        },
+        19160: (e, t, r) => {
+            var n = r(85),
+                o = r(4137),
+                u = r(31958),
+                l = n ? n.toStringTag : void 0;
+            e.exports = function(e) {
+                return null == e ? void 0 === e ? "[object Undefined]" : "[object Null]" : l && l in Object(e) ? o(e) : u(e)
+            }
+        },
+        23712: (e, t, r) => {
+            var n = r(78361),
+                o = function() {
+                    var e = /[^.]+$/.exec(n && n.keys && n.keys.IE_PROTO || "");
+                    return e ? "Symbol(src)_1." + e : ""
+                }();
+            e.exports = function(e) {
+                return !!o && o in e
+            }
+        },
+        24956: (e, t, r) => {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "workAsyncStorageInstance", {
+                enumerable: !0,
+                get: function() {
+                    return n
+                }
+            });
+            let n = (0, r(64190).createAsyncLocalStorage)()
+        },
+        31958: e => {
+            var t = Object.prototype.toString;
+            e.exports = function(e) {
+                return t.call(e)
+            }
+        },
+        33637: e => {
+            var t = Function.prototype.toString;
+            e.exports = function(e) {
+                if (null != e) {
+                    try {
+                        return t.call(e)
+                    } catch (e) {}
+                    try {
+                        return e + ""
+                    } catch (e) {}
+                }
+                return ""
+            }
+        },
+        36143: (e, t, r) => {
+            "use strict";
+            r.d(t, {
+                W: () => u
+            });
+            var n = r(53163),
+                o = r(36309);
+
+            function u(e, {
+                root: t,
+                margin: r,
+                amount: l,
+                once: a = !1,
+                initial: i = !1
+            } = {}) {
+                let [c, s] = (0, n.useState)(i);
+                return (0, n.useEffect)(() => {
+                    if (!e.current || a && c) return;
+                    let n = {
+                        root: t && t.current || void 0,
+                        margin: r,
+                        amount: l
+                    };
+                    return (0, o.l)(e.current, () => (s(!0), a ? void 0 : () => s(!1)), n)
+                }, [t, e, r, a, l]), c
+            }
+        },
+        36223: (e, t, r) => {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "default", {
+                enumerable: !0,
+                get: function() {
+                    return o
+                }
+            });
+            let n = r(54994)._(r(91511));
+
+            function o(e, t) {
+                var r;
+                let o = {};
+                "function" == typeof e && (o.loader = e);
+                let u = {
+                    ...o,
+                    ...t
+                };
+                return (0, n.default)({
+                    ...u,
+                    modules: null == (r = u.loadableGenerated) ? void 0 : r.modules
+                })
+            }("function" == typeof t.default || "object" == typeof t.default && null !== t.default) && void 0 === t.default.__esModule && (Object.defineProperty(t.default, "__esModule", {
+                value: !0
+            }), Object.assign(t.default, t), e.exports = t.default)
+        },
+        36309: (e, t, r) => {
+            "use strict";
+            r.d(t, {
+                l: () => u
+            });
+            var n = r(74247);
+            let o = {
+                some: 0,
+                all: 1
+            };
+
+            function u(e, t, {
+                root: r,
+                margin: l,
+                amount: a = "some"
+            } = {}) {
+                let i = (0, n.K)(e),
+                    c = new WeakMap,
+                    s = new IntersectionObserver(e => {
+                        e.forEach(e => {
+                            let r = c.get(e.target);
+                            if (!!r !== e.isIntersecting)
+                                if (e.isIntersecting) {
+                                    let r = t(e.target, e);
+                                    "function" == typeof r ? c.set(e.target, r) : s.unobserve(e.target)
+                                } else "function" == typeof r && (r(e), c.delete(e.target))
+                        })
+                    }, {
+                        root: r,
+                        rootMargin: l,
+                        threshold: "number" == typeof a ? a : o[a]
+                    });
+                return i.forEach(e => s.observe(e)), () => s.disconnect()
+            }
+        },
+        39340: (e, t, r) => {
+            "use strict";
+
+            function n(e) {
+                let {
+                    reason: t,
+                    children: r
+                } = e;
+                return r
+            }
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "BailoutToCSR", {
+                enumerable: !0,
+                get: function() {
+                    return n
+                }
+            }), r(34496)
+        },
+        39360: (e, t, r) => {
+            e.exports = "object" == typeof r.g && r.g && r.g.Object === Object && r.g
+        },
+        41796: (e, t, r) => {
+            var n = r(19160),
+                o = r(67887);
+            e.exports = function(e) {
+                if (!o(e)) return !1;
+                var t = n(e);
+                return "[object Function]" == t || "[object GeneratorFunction]" == t || "[object AsyncFunction]" == t || "[object Proxy]" == t
+            }
+        },
+        42898: (e, t, r) => {
+            "use strict";
+            r.d(t, {
+                default: () => o.a
+            });
+            var n = r(36223),
+                o = r.n(n)
+        },
+        55368: (e, t, r) => {
+            var n = r(78939),
+                o = r(76592);
+            e.exports = function(e, t) {
+                var r = o(e, t);
+                return n(r) ? r : void 0
+            }
+        },
+        64190: (e, t) => {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), ! function(e, t) {
+                for (var r in t) Object.defineProperty(e, r, {
+                    enumerable: !0,
+                    get: t[r]
+                })
+            }(t, {
+                bindSnapshot: function() {
+                    return l
+                },
+                createAsyncLocalStorage: function() {
+                    return u
+                },
+                createSnapshot: function() {
+                    return a
+                }
+            });
+            let r = Object.defineProperty(Error("Invariant: AsyncLocalStorage accessed in runtime where it is not available"), "__NEXT_ERROR_CODE", {
+                value: "E504",
+                enumerable: !1,
+                configurable: !0
+            });
+            class n {
+                disable() {
+                    throw r
+                }
+                getStore() {}
+                run() {
+                    throw r
+                }
+                exit() {
+                    throw r
+                }
+                enterWith() {
+                    throw r
+                }
+                static bind(e) {
+                    return e
+                }
+            }
+            let o = "undefined" != typeof globalThis && globalThis.AsyncLocalStorage;
+
+            function u() {
+                return o ? new o : new n
+            }
+
+            function l(e) {
+                return o ? o.bind(e) : n.bind(e)
+            }
+
+            function a() {
+                return o ? o.snapshot() : function(e, ...t) {
+                    return e(...t)
+                }
+            }
+        },
+        64234: (e, t, r) => {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "workAsyncStorage", {
+                enumerable: !0,
+                get: function() {
+                    return n.workAsyncStorageInstance
+                }
+            });
+            let n = r(24956)
+        },
+        67887: e => {
+            e.exports = function(e) {
+                var t = typeof e;
+                return null != e && ("object" == t || "function" == t)
+            }
+        },
+        76592: e => {
+            e.exports = function(e, t) {
+                return null == e ? void 0 : e[t]
+            }
+        },
+        78361: (e, t, r) => {
+            e.exports = r(9593)["__core-js_shared__"]
+        },
+        78939: (e, t, r) => {
+            var n = r(41796),
+                o = r(23712),
+                u = r(67887),
+                l = r(33637),
+                a = /^\[object .+?Constructor\]$/,
+                i = Object.prototype,
+                c = Function.prototype.toString,
+                s = i.hasOwnProperty,
+                f = RegExp("^" + c.call(s).replace(/[\\^$.*+?()[\]{}|]/g, "\\$&").replace(/hasOwnProperty|(function).*?(?=\\\()| for .+?(?=\\\])/g, "$1.*?") + "$");
+            e.exports = function(e) {
+                return !(!u(e) || o(e)) && (n(e) ? f : a).test(l(e))
+            }
+        },
+        79248: (e, t, r) => {
+            var n = r(19160),
+                o = r(8472);
+            e.exports = function(e) {
+                return "symbol" == typeof e || o(e) && "[object Symbol]" == n(e)
+            }
+        },
+        87585: (e, t, r) => {
+            "use strict";
+
+            function n(e) {
+                let {
+                    moduleIds: t
+                } = e;
+                return null
+            }
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "PreloadChunks", {
+                enumerable: !0,
+                get: function() {
+                    return n
+                }
+            }), r(32475), r(31114), r(64234), r(74399)
+        },
+        90269: (e, t, r) => {
+            e.exports = r(55368)(r(9593), "Map")
+        },
+        91511: (e, t, r) => {
+            "use strict";
+            Object.defineProperty(t, "__esModule", {
+                value: !0
+            }), Object.defineProperty(t, "default", {
+                enumerable: !0,
+                get: function() {
+                    return i
+                }
+            });
+            let n = r(32475),
+                o = r(53163),
+                u = r(39340);
+
+            function l(e) {
+                return {
+                    default: e && "default" in e ? e.default : e
+                }
+            }
+            r(87585);
+            let a = {
+                    loader: () => Promise.resolve(l(() => null)),
+                    loading: null,
+                    ssr: !0
+                },
+                i = function(e) {
+                    let t = {
+                            ...a,
+                            ...e
+                        },
+                        r = (0, o.lazy)(() => t.loader().then(l)),
+                        i = t.loading;
+
+                    function c(e) {
+                        let l = i ? (0, n.jsx)(i, {
+                                isLoading: !0,
+                                pastDelay: !0,
+                                error: null
+                            }) : null,
+                            a = !t.ssr || !!t.loading,
+                            c = a ? o.Suspense : o.Fragment,
+                            s = t.ssr ? (0, n.jsxs)(n.Fragment, {
+                                children: [null, (0, n.jsx)(r, {
+                                    ...e
+                                })]
+                            }) : (0, n.jsx)(u.BailoutToCSR, {
+                                reason: "next/dynamic",
+                                children: (0, n.jsx)(r, {
+                                    ...e
+                                })
+                            });
+                        return (0, n.jsx)(c, {
+                            ...a ? {
+                                fallback: l
+                            } : {},
+                            children: s
+                        })
+                    }
+                    return c.displayName = "LoadableComponent", c
+                }
+        },
+        97557: e => {
+            e.exports = Array.isArray
+        }
+    }
+]);
